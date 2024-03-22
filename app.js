1976aec8d2290f900830721747cee4a3964a33e4
Emma Wood <emmawood.coder@gmail.com>
2024-03-22 10:46:39 -0600

    first push

---
+++ app.js
@@ -0,0 +1,17 @@
+let clickUpgrades = [
+	{
+		name: 'pickaxe',
+		price: 100,
+		quantity: 0,
+		multiplier: 1
+	}
+];
+
+let automaticUpgrades = [
+	{
+		name: 'rover',
+		price: 600,
+		quantity: 0,
+		multiplier: 20
+	}
+];
